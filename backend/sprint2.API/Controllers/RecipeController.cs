f416d1de3ef57021d55f2095debf2e979e995e5b
Ella Neumarker <eneumarker@gmail.com>
2025-03-25 17:31:18 -0600

    Added Recipe Controller and DbContext

--- backend/sprint2.API/Controllers/RecipeController.cs
+++ backend/sprint2.API/Controllers/RecipeController.cs
@@ -1,6 +1,30 @@
-namespace sprint2.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sprint2.API.Data;
 
-public class RecipeController
+namespace sprint2.API.Controllers
 {
     
+    [Route("[controller]")]
+    [ApiController]
+    
+    public class RecipeController : ControllerBase
+    {
+        private RecipesContext _recipesContext;
+
+        public RecipeController(RecipesContext recipesContext)
+        {
+            _recipesContext = recipesContext;
+        }
+
+        [HttpGet(Name = "GetRecipes")]
+        public IEnumerable<Recipe> Get()
+        {
+
+            var recipesList = _recipesContext.Recipes;
+            
+            return (recipesList);
+        }
+    
+    }
 }
