38a42a69b96529c0093947b62a173606bc8a1793
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-03-26 12:46:14 -0600

    Add calendar view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import './App.css';
-import NavBar from './component/NavBar';
-import Homepage from './home';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Loading from './loading';
-import Recipe from './recipe';
-import RecipeSearch from './recipeSearch';
-import SignUp from './signup';
-import Stats from './stats';
-import Today from './today';
-import { useEffect, useState } from 'react';
-import Login from './login';
-import { RecipeListProvider } from './context/RecipeListContext';
+import "./App.css";
+import NavBar from "./component/NavBar";
+import Homepage from "./home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Loading from "./loading";
+import Recipe from "./recipe";
+import RecipeSearch from "./recipeSearch";
+import SignUp from "./signup";
+import Stats from "./stats";
+import Today from "./today";
+import { useEffect, useState } from "react";
+import Login from "./login";
+import { RecipeListProvider } from "./context/RecipeListContext";
+import CalendarMonth from "./calendarMonth";
 
 function App() {
   const [data, setData] = useState(null);
@@ -24,11 +30,11 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.example.com/data');
+        const response = await fetch("https://api.example.com/data");
         const result = await response.json();
         setData(result);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       } finally {
         setIsLoading(false);
       }
@@ -41,30 +47,29 @@ function App() {
       <RecipeListProvider>
         {!isLoggedIn ? (
           <Router>
-              <Routes>
-                <Route path="/login" element={<Login onLogin={handleLogin}/>} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="*" element={<Navigate to="/login" />} />
-              </Routes>
-            </Router>
+            <Routes>
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </Router>
+        ) : isLoading ? (
+          <Loading />
         ) : (
-          isLoading ? (
-            <Loading />
-          ) : (
-            <Router>
-              <Routes>
-                <Route path="/" element={<Navigate to="/home" />} />
-                <Route path="/login" element={<Navigate to="/home" />} />
-                <Route path="/home" element={<Homepage data={data} />} />
-                <Route path="/recipe/:recipeId" element={<Recipe />} />
-                <Route path="/search" element={<RecipeSearch />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/stats" element={<Stats />} />
-                <Route path="/today" element={<Today />} />
-              </Routes>
-              <NavBar />
-            </Router>
-          )
+          <Router>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/login" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Homepage data={data} />} />
+              <Route path="/recipe/:recipeId" element={<Recipe />} />
+              <Route path="/search" element={<RecipeSearch />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/stats" element={<Stats />} />
+              <Route path="/today" element={<Today />} />
+              <Route path="/calendarMonth" element={<CalendarMonth />} />
+            </Routes>
+            <NavBar />
+          </Router>
         )}
       </RecipeListProvider>
     </>
